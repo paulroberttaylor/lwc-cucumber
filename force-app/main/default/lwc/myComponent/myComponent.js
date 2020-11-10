aaa75a14d367e94004b67086aec64b3a82ba45b1
Paul <hello@sfdcpaul.com>
2020-11-10 13:28:07 +0000

    Remove console log.

--- force-app/main/default/lwc/myComponent/myComponent.js
+++ force-app/main/default/lwc/myComponent/myComponent.js
@@ -3,9 +3,7 @@ import { LightningElement } from 'lwc';
 export default class MyComponent extends LightningElement {
 
     handleClick() {
-
-        console.log('handleClick');
-
+        
         this.dispatchEvent(
             new CustomEvent('myevent', {
                 detail: {
